5e7a67c7cc9543b408534c76286cb5da77fae53f
이아현 <suk03029@gamil.com>
2021-11-16 16:04:53 +0900

    파일 경로와 파일 작성 클래스 소환
    생성자 단의 에러발생

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,26 @@
 package codes;
 
+import java.io.File;
+import java.io.FileWriter;
+
 public class MainDrive {
-public static void main(String[] args) {
+	public static void main(String[] args) {
+
+	writeToFile();
 	
+		
+	}
 	
-	
-}
+	static void writeToFile() {
+//		파일 저장 (output) 실습
+		
+//	1. 어디에 파일을 저장 할 것인지 경로 설정을 해야 함
+		File myFile = new File("myTest.txt");
+		
+//	2. 실제로 파일 작성을 도와주는 클래스 활용
+		
+		FileWriter fw = new FileWriter(myFile);
+		
+		
+	}
 }
